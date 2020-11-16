916050c1555439dc036aa2dd7e55bc44cae5015d
Bruno Ascenso <bascenso@gmail.com>
2020-11-16 11:28:10 +0000

    Data update

--- R/main.R
+++ R/main.R
@@ -12,11 +12,11 @@ source("storage.R", encoding = "utf-8")
 source("maps.R", encoding = "utf-8")
 source("dump.R", encoding = "utf-8")
 
-## Personal PC folder
+## PC 1 folder
 ##setwd("c:/Drive/OneDrive/11.GitRepos/alkateia/R/")
 
-## Nokia PC folder
-## setwd("C:/Users/ascenso/OneDrive - Nokia/99.Repos/R")
+## PC 2 folder
+## setwd("C:/Users/basce/Documents/Repos/alkateia/R")
 
 ## ===========================================================================================================
 ## Get clan data and build stats
@@ -40,10 +40,10 @@ clan$stats <- logClanStats(clan$memberInfo, clanStatsFile)
 ## War stats
 cat("Building war stats... ")
 
-clan$warStats <- buildWarStats(clan, nwars = 60)
+clan$warStats <- buildWarStats(clan, nwars = 50)
 
 ## War participation map
-warParticipationDF <- buildWarMap(clan, nwars = 60)
+warParticipationDF <- buildWarMap(clan, nwars = 50)
 
 ## Performance evolution map
 evolutionDF <- buildEvolutionMap(clan, nperiod = 3, warsPerPeriod = 15)
